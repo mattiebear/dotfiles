2b0851f3db650d2e2d66cb6cedc14fe64134b924
Matthew Sells <matthew.sells@proton.me>
2024-09-18 09:04:54 -0500

    Display hidden files in fuzzy finder

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,7 +1,9 @@
 local telescope = require("telescope.builtin")
 
 -- File search
-vim.keymap.set("n", "<leader>ff", telescope.find_files, {})
+vim.keymap.set("n", "<leader>ff", function()
+	telescope.find_files({ hidden = true })
+end, {})
 vim.keymap.set("n", "<leader>fg", telescope.live_grep, {})
 vim.keymap.set("n", "<leader>fb", telescope.buffers, {})
 vim.keymap.set("n", "<leader>fh", telescope.help_tags, {})
