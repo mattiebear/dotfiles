b3494197f76e416abee753bc0bff2aafdb6ceb4e
Matthew Sells <matthew.sells@proton.me>
2025-03-18 14:49:48 -0500

    add surround formatting

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,31 +1,34 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>f",
-			function()
-				require("conform").format({ async = true })
-			end,
-			mode = "",
-			desc = "Format buffer",
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
 		},
+		opts = {
+			formatters_by_ft = {
+				css = { "prettier" },
+				lua = { "stylua" },
+				elixir = { "mix" },
+				eelixir = { "mix" },
+				heex = { "mix" },
+				svelte = { "prettier" },
+				yml = { "yamlfmt" },
+				yaml = { "yamlfmt" },
+			},
+			default_format_opts = {
+				lsp_format = "fallback",
+			},
+			format_on_save = { timeout_ms = 3000 },
+		}
 	},
-	opts = {
-		formatters_by_ft = {
-			css = { "prettier" },
-			lua = { "stylua" },
-			elixir = { "mix" },
-			eelixir = { "mix" },
-			heex = { "mix" },
-			svelte = { "prettier" },
-			yml = { "yamlfmt" },
-			yaml = { "yamlfmt" },
-		},
-		default_format_opts = {
-			lsp_format = "fallback",
-		},
-		format_on_save = { timeout_ms = 3000 },
-	},
+	{ "tpope/vim-surround" }
 }
